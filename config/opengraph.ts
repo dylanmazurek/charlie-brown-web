916a568c8f6af7b1cb48028c29785e67d7568e7a
dylanmazurek <dylmazurek@gmail.com>
2025-03-08 18:01:22 +1100

    Update Open Graph configuration to use show-specific details

--- config/opengraph.ts
+++ config/opengraph.ts
@@ -3,16 +3,16 @@ import { siteConfig } from "./site";
 
 export const openGraph: OpenGraph = {
   type: "website",
-  url: siteConfig.url,
-  title: siteConfig.title,
-  description: siteConfig.description,
-  siteName: siteConfig.name,
+  url: siteConfig.site.url,
+  title: siteConfig.show.name,
+  description: siteConfig.show.description,
+  siteName: siteConfig.show.name,
   images: [
     {
       url: "/og-charlie-image.png",
       width: 1200,
       height: 630,
-      alt: siteConfig.name,
+      alt: siteConfig.show.name,
     }
   ],
 }
